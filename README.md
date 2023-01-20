c92c23f8de2e206266940275d474383e0c78df5e
Rafael Kübler da Silva <rafael.silva@netlight.com>
2023-01-20 12:05:06 +0100

    Day 2

---
+++ README.md
@@ -0,0 +1,13 @@
+# Advent of Code 2022
+
+This repo contains my solutions for the [Advent of Code 2022](https://adventofcode.com/2022).
+
+From the website:
+
+> Advent of Code is an Advent calendar of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like. People use them as interview prep, company training, university coursework, practice problems, a speed contest, or to challenge each other.
+
+## Running
+
+The `run.py` files are supposed to be run from the repo root.
+
+The solutions are all written in `Python 3.8`. Newer Python versions have been out, but I just happened to have this one installed at the time.
---
+++ day2/run.py
@@ -0,0 +1,38 @@
+from typing import List
+import math
+
+
+def solve(lines: List[str]) -> int:
+    total_paper = 0
+
+    for line in lines:
+        l, w, h = [int(num) for num in line.split("x")]
+    
+        sides = [l*w, w*h, h*l]
+        total_paper += sum(sides) * 2 + min(sides)
+
+    return total_paper
+
+def solve2(lines: List[str]) -> int:
+    total_ribbon = 0
+
+    for line in lines:
+        l, w, h = [int(num) for num in line.split("x")]
+    
+        sides = sorted([l, w, h])
+        total_ribbon += sum(sides[:2]) * 2 + l*w*h
+
+    return total_ribbon
+
+
+if __name__ == "__main__":
+    with open("day2/input.txt") as fd:
+        lines: List[str] = [line.strip() for line in fd.readlines()]
+
+    solution1 = solve(lines)
+    print(solution1)
+    #assert solution1 == 1606483
+
+    solution2 = solve2(lines)
+    print(solution2)
+    #assert solution2 == 3842356
