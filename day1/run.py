9b1b32678c0bad46215596126d54b013baf9dba2
Rafael Kübler da Silva <rafael.silva@netlight.com>
2023-01-20 10:38:03 +0100

    Day 1

---
+++ day1/run.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+def solve(chars: str, part2: bool = False) -> int:
+    floor = 0
+    
+    for i, char in enumerate(chars):
+        if char == "(": floor += 1
+        else: floor -= 1
+
+        if part2 and floor == -1:
+            return i + 1
+
+    return floor
+
+
+if __name__ == "__main__":
+    with open("day1/input.txt") as fd:
+        chars: List[str] = fd.readline().strip()
+
+    solution1 = solve(chars)
+    print(solution1)
+    assert solution1 == 280
+
+    solution2 = solve(chars, True)
+    print(solution2)
+    assert solution2 == 1797
