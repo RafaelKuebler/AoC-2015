85734b415d819b54cfd7fd1a46cadd9cacd4200c
Rafael Kübler da Silva <rafael.silva@netlight.com>
2023-04-25 22:17:11 +0200

    Day 24

---
+++ day24/run.py
@@ -0,0 +1,48 @@
+from typing import List, Set
+from itertools import combinations
+import math
+
+
+# TODO: recursion would be a lot cleaner!
+
+def entanglement(packages: Set[int], part2: bool = False) -> int:
+    total = sum(packages)
+    goal_weight = total / (4 if part2 else 3)
+
+    for i in range(1, len(packages)):
+        for combi in combinations(packages, i):
+            if sum(combi) != goal_weight:
+                continue
+            
+            for j in range(1, len(packages)-i):
+                options: List[List[int]] = []
+                for combi2 in combinations(packages - set(combi), j):
+                    if sum(combi2) != goal_weight:
+                        continue
+                    if not part2:
+                        options.append(combi)
+                        continue
+
+                    for k in range(1, len(packages)-i-j):
+                        options = []
+                        for combi3 in combinations(packages - set(combi) - set(combi2), k):
+                            if sum(combi3) == goal_weight:
+                                options.append(combi)
+                        if options:
+                            return min(math.prod(x) for x in options)
+                            
+                if not part2 and options:
+                    return min(math.prod(x) for x in options)
+
+
+if __name__ == "__main__":
+    with open("day24/input.txt") as fd:
+        packages = {int(line.strip()) for line in fd.readlines()}
+
+    solution1 = entanglement(packages)
+    print(solution1)
+    assert solution1 == 11846773891
+
+    solution2 = entanglement(packages, part2=True)
+    print(solution2)
+    assert solution2 == 80393059
